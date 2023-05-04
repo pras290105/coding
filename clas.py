4c4823d8e8efde46ec920486b8c92e844e7988a8
prasetya <prasetyaesaputra@gmail.com>
2023-05-04 15:15:21 +0700

    first commit

---
+++ clas.py
@@ -0,0 +1,10 @@
+class Hero:
+    def __init__(self,name):
+        self.name = name
+
+a = Hero("uyu")
+    
+print(a.name)
+
+
+
---
+++ tes2.py
@@ -0,0 +1,8 @@
+import tes
+a = True
+b = False
+c = False
+if a or b and c:
+    print("bnr")
+else:
+    print("salah")

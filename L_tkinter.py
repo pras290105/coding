b47fc15e0490cfabaa42df2806e5bb4d869bd415
prasetya <prasetyaesaputra@gmail.com>
2023-05-17 18:32:35 +0700

    ioo

---
+++ L_tkinter.py
@@ -0,0 +1,9 @@
+import tkinter
+window = tkinter.Tk()
+label1 = tkinter.Label(window, text= "pras1")
+label1.pack()
+tombol = tkinter.Button(window,text="kanjut")
+tombol.pack()
+label2 = tkinter.Button(window,text="ini kanjut")
+label2.pack()
+window.mainloop()
